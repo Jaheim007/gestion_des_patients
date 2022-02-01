b27167a850545042cbd7804005ee92e547ae93cb
iMac-13 <imac-13@iMac-de-iMac-13.local>
2022-02-01 14:23:52 +0000

    continues

--- modules/fonctions.py
+++ modules/fonctions.py
@@ -1,6 +1,6 @@
-from multiprocessing.dummy import connection
-from textwrap import fill
+from email import message
 from tkinter import*
+from tkinter import messagebox
 import page
 import json
 
@@ -14,19 +14,47 @@ def inscription():
     frame = Frame(inscription, highlightbackground="black", highlightthickness=2)
     frame.pack(padx=70, pady=(50),)
 
-    Nom = Label(frame, text=" Nom ",font="Roboto 15").grid(row=0, column=0, sticky=E )
-    nom_entry = Entry(frame).grid(row=0, column=1, ipadx=25, ipady=5, sticky= E)
-    Prenom = Label(frame, text=" Prenom ",font="Roboto 15").grid(row=1, column=0, sticky=E)
-    prenom_entry = Entry(frame).grid(row=1, column=1 ,ipadx=25, ipady=5, sticky= E) 
-    Email = Label(frame, text=" Email ",font="Roboto 15").grid(row=2, column=0, sticky= E)
-    email_entry = Entry(frame).grid(row=2, column=1, ipadx=25, ipady=5, sticky=E )
+    Nom = Label(frame, text=" Nom ",font="Roboto 15")
+    Nom.grid(row=0, column=0, sticky=E )
+    nom_entry = Entry(frame)
+    nom_entry.grid(row=0, column=1, ipadx=25, ipady=5, sticky= E)
     
+    Prenom = Label(frame, text=" Prenom ",font="Roboto 15")
+    Prenom.grid(row=1, column=0, sticky=E)
+    prenom_entry = Entry(frame)
+    prenom_entry.grid(row=1, column=1 ,ipadx=25, ipady=5, sticky= E) 
+    
+    Email = Label(frame, text=" Email ",font="Roboto 15")
+    Email.grid(row=2, column=0, sticky= E)
+    email_entry = Entry(frame)
+    email_entry.grid(row=2, column=1, ipadx=25, ipady=5, sticky=E)
+    
+    def save1():       
+        dict ={
+            "Nom": nom_entry.get(),   
+            "Prenom":prenom_entry.get(),   
+            "Email": email_entry.get()   
+        }
         
-    btn = Button(frame, text="S'Inscrire", width=10, height=2, font="Roboto 15").grid(row=3, columnspan=2, sticky="N")
+        with open("data.json", "a") as f:   
+            json.dump(dict,f ,indent=4)
+            
+            
+        if nom_entry.get()=="" or prenom_entry.get()=="" or email_entry.get() =="":       
+            messagebox.showerror("Error", " Veuillez saisir vos informations. ")
+            
+        else:     
+            messagebox.showinfo("Info","Vous êtes inscrit" )  
+            
+        nom_entry.delete(0,END)
+        prenom_entry.delete(0,END)
+        email_entry.delete(0,END)
+           
+    btn = Button(frame, text="S'Inscrire",command=save1 , width=10, height=2, font="Roboto 15").grid(row=3, columnspan=2, sticky="N")
     
     inscription.mainloop()
 
-def connexion():      
+def connexion(): 
     connexion = Tk()
     connexion.title("Gestion Des Patients")
     connexion.resizable(False, False)
@@ -36,11 +64,28 @@ def connexion():
     frame = Frame(connexion, highlightbackground="black", highlightthickness=2)
     frame.pack(pady=50)
 
-    username = Label(frame, text=" Username ",font="Roboto 15").grid(row=0, column=0, sticky=E )
-    username_entry = Entry(frame).grid(row=0, column=1, ipadx=25, ipady=5, sticky= E)
-    Email = Label(frame, text=" Email ",font="Roboto 15").grid(row=1, column=0, sticky=E)
-    Email_entry = Entry(frame).grid(row=1, column=1 ,ipadx=25, ipady=5, sticky= E) 
+    nom = Label(frame, text=" Nom ",font="Roboto 15").grid(row=0, column=0, sticky=E)
+    nom_entry = Entry(frame).grid(row=0, column=1, ipadx=25, ipady=5, sticky= E)
     
-    btn = Button(frame, text ="Se Connecter", font= "Roboto 15" ,command= page.form, width=10, height=2).grid(row=3, columnspan=2, sticky="N")
+    Prenom = Label(frame, text=" Prenom ",font="Roboto 15").grid(row=1, column=0, sticky=E)
+    prenom_entry = Entry(frame).grid(row=1, column=1 ,ipadx=25, ipady=5, sticky= E) 
+    
+    Email = Label(frame, text=" Email ",font="Roboto 15").grid(row=2, column=0, sticky=E)
+    Email_entry = Entry(frame).grid(row=2, column=1 ,ipadx=25, ipady=5, sticky= E) 
+    
+    def data():       
+        try:   
+            global dict 
+            with open("data.json", "r") as f:
+                file = json.load(f)
+                
+        except json.JSONDecodeError:    
+            if nom_entry and prenom_entry and Email_entry not in file:      
+                messagebox.showerror("Error", "Vous n'êtes pas inscrit.")
+        
+        else:           
+            messagebox.showinfo("ShowInfo", "Bienvenue") 
+        
+    btn = Button(frame, text ="Se Connecter", font= "Roboto 15" ,command=data, width=10, height=2).grid(row=3, columnspan=2, sticky="N")
      
     connexion.mainloop()
--- page.py
+++ page.py
@@ -13,10 +13,12 @@ def form():
     frame = Frame(form, highlightbackground= "black", highlightthickness=2)
     frame.pack(pady=50, ipadx=100, ipady=50)
     
-    Nom = Label(frame, text=" Nom ",font="Roboto 15", justify="right").pack(pady=10)
-    nom_entry = Entry(frame, textvariable=StringVar()).pack(pady=3)
-    Prenom = Label(frame, text=" Prenom ",font="Roboto 15").pack(pady=10)
-    prenom_entry = Entry(frame, textvariable=StringVar()).pack(pady=3)
+    Nom = Label(frame, text=" Nom ",font="Roboto 15", )
+    Nom.pack(pady=10)
+    nom_entry = Entry(frame).pack(pady=3)
+    Prenom = Label(frame, text=" Prenom ",font="Roboto 15")
+    Prenom.pack(pady=10)
+    prenom_entry = Entry(frame).pack(pady=3)
     
     dif_age = [
         "0","1","2","3",
@@ -69,64 +71,67 @@ def form():
     
     drop1 = StringVar(frame) 
     drop1.set("Entrer l'âge du patient")
-    age = Label(frame, text="Age",font="Roboto 15").pack(pady=10)
+    age = Label(frame, text="Age",font="Roboto 15")
+    age.pack(pady=10)
     age_drop = OptionMenu(frame , drop1 , *dif_age).pack(ipadx=25, pady=5)
     
-    problème_médical = Label(frame, text="Problème Médical",font="Roboto 15").pack(pady=10)
-    problème_médical_entry = Entry(frame, textvariable=StringVar()).pack(pady=5, ipadx=15)
+    problème_médical = Label(frame, text="Problème Médical",font="Roboto 15")
+    problème_médical.pack(pady=10)
+    problème_médical_entry = Entry(frame,).pack(pady=5, ipadx=15)
    
     drop2 = StringVar(frame) 
     drop2.set("Déjà traité ")
-    status= Label(frame, text="Statut du patient",font="Roboto 15", ).pack(pady=10)
+    status= Label(frame, text="Statut du patient",font="Roboto 15",)
+    status.pack(pady=10)
     status_drop = OptionMenu(frame, drop2, *dif_wounds).pack(ipadx=50, pady=5)
     
     drop3 = StringVar(frame) 
     drop3.set("Médecin à voir")
-    medecin = Label(frame, text="Les Différents Médecins",font="Roboto 15", ).pack(pady=10)
+    medecin = Label(frame, text="Les Différents Médecins",font="Roboto 15", )
+    medecin.pack(pady=10)
     medecin_drop = OptionMenu(frame, drop3, *dif_doctor,).pack(ipadx=50, pady=5)
     
     drop4 = StringVar(frame) 
     drop4.set("Chambre de patient")
-    chambre = Label(frame, text="Salle De Repos",font="Roboto 15").pack(pady=10)
+    chambre = Label(frame, text="Salle De Repos",font="Roboto 15")
+    chambre.pack(pady=10)
     chambre_drop = OptionMenu(frame, drop4, *dif_chambre,).pack(ipadx=50, pady=5)
     
-    """def check():
-        a = nom_entry.get()
-        b = prenom_entry.get()
-        c = age_drop.get()
-        d = problème_médical_entry.get()
-        e = status_drop.get()
-        f = medecin_drop.get()
-        g = chambre_drop.get()
-        
-        print(a)
-        print(b)
-        print(c)
-        print(d)
-        print(e)
-        print(e)
-        print(f)
-        print(g)
-        
-        data = {}
-        data['Nom'] = a
-        data['Prenom'] = b
-        data['Age'] = c
-        data['Problème_médical'] = d
-        data['Statut'] = e
-        data['Medecin'] = f
-        data['Chambre'] = f
-        i = 0
-        out_file = open("data.json", "w")
-        json.dump(data, out_file)
-        tree.insert('', index='end', 
-        values =(nom_entry.get(),prenom_entry.get(),age_drop.get()
-        ,problème_médical_entry.get() ,status_drop.get() ,medecin_drop.get(),chambre_drop.get()))  
-        i = i+1
-        messagebox.showinfo('SAVED')
-        out_file.close()"""
-    btn = Button(frame, text="Enregistrer Le Patient", font="Roboto 15",).pack(ipadx=25, pady=10, ipady=10)    
-
+    nom_entry = StringVar()
+    prenom_entry = StringVar()
+    age_drop = StringVar()
+    problème_médical_entry = StringVar()
+    status_drop = StringVar()
+    medecin_drop = StringVar()
+    chambre_drop = StringVar()
+     
+    nom1 = nom_entry.get()
+    prenom1 = prenom_entry.get()
+    age1 = age_drop.get()
+    pro1 = problème_médical_entry.get()
+    stat1 = status_drop.get()
+    med1 = medecin_drop.get()
+    room1 = chambre_drop.get()
+    
+    def save():       
+          
+        dict ={
+            "Nom": nom1,
+            "Prenom": prenom1,
+            "Age": age1,
+            "Probleme_medical": pro1,
+            "Statuts": stat1,
+            "Medecin": med1,
+            "Chambre": room1
+            }
+        with open("form.json", "a") as f:   
+            json.dump(dict,f ,indent=4)
+        print(dict)
+    btn= Button(frame, text="Enregistrer Le Patient", command=save, font="Roboto 15",)
+    btn.pack(ipadx=25, pady=10, ipady=10) 
+    
+    form.mainloop()
+    
 def tree():
     window = Tk()
     window.title("Gestion des Stocke")
